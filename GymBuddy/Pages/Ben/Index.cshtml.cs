44af6a6301376cc20841a088d8878548a2b610b2
simpyb761 <simpyb761@gmail.com>
2023-09-18 12:25:32 -0400

    Changed Exercise to Ben
    Created a folder named Ben for views and deleted Exercise views template.

---
+++ GymBuddy/Pages/Ben/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using GymBuddy.Data;
+using GymBuddy.Models;
+
+namespace GymBuddy.Pages.Ben
+{
+    public class IndexModel : PageModel
+    {
+        private readonly GymBuddy.Data.GymBuddyContext _context;
+
+        public IndexModel(GymBuddy.Data.GymBuddyContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Exercises> Exercises { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Exercises != null)
+            {
+                Exercises = await _context.Exercises.ToListAsync();
+            }
+        }
+    }
+}
