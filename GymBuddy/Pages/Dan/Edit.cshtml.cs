14efd4599c4867214bc454d73c554c8f6937e89d
danielchoike2 <choike3@mail.nmc.edu>
2023-09-18 13:57:59 -0400

    Dan Update 1

---
+++ GymBuddy/Pages/Dan/Edit.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GymBuddy.Data;
+using GymBuddy.Models;
+
+namespace GymBuddy.Pages.Dan
+{
+    public class EditModel : PageModel
+    {
+        private readonly GymBuddy.Data.GymBuddyContext _context;
+
+        public EditModel(GymBuddy.Data.GymBuddyContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Exercises Exercises { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Exercises == null)
+            {
+                return NotFound();
+            }
+
+            var exercises =  await _context.Exercises.FirstOrDefaultAsync(m => m.Id == id);
+            if (exercises == null)
+            {
+                return NotFound();
+            }
+            Exercises = exercises;
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Exercises).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ExercisesExists(Exercises.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool ExercisesExists(int id)
+        {
+          return (_context.Exercises?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
