da306e7975a2e9b97d74b51580923c73eca3dc58
simpyb761 <simpyb761@gmail.com>
2023-09-18 13:42:18 -0400

    Added search and filter

--- GymBuddy/Pages/Ben/Index.cshtml.cs
+++ GymBuddy/Pages/Ben/Index.cshtml.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using GymBuddy.Data;
 using GymBuddy.Models;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.View;
 
 namespace GymBuddy.Pages.Ben
 {
@@ -20,13 +22,30 @@ namespace GymBuddy.Pages.Ben
         }
 
         public IList<Exercises> Exercises { get;set; } = default!;
+        [BindProperty(SupportsGet =true)]
+        public string? SearchString { get; set; }
+        public SelectList  ExperienceLevel { get; set; }
+        [BindProperty(SupportsGet =true)]
+        public string? UserExperience { get; set; }
 
         public async Task OnGetAsync()
         {
-            if (_context.Exercises != null)
+            IQueryable<GymBuddy.Models.TrainingLevel> experienceQuery = from m in _context.Exercises
+                                                 orderby m.TrainingLevel
+                                                 select m.TrainingLevel;
+            var exercises = from m in _context.Exercises
+                         select m;
+            if (!string.IsNullOrEmpty(SearchString))
             {
-                Exercises = await _context.Exercises.ToListAsync();
+                exercises = exercises.Where(s => s.Name.Contains(SearchString));
             }
+            if (!string.IsNullOrEmpty(UserExperience))
+            {
+                exercises = exercises.Where(x => x.TrainingLevel == Enum.Parse<TrainingLevel>(UserExperience));
+            }
+                ExperienceLevel = new SelectList(await experienceQuery.Distinct().ToListAsync());
+                Exercises = await exercises.ToListAsync();
+            
         }
     }
 }
